291c861f8f6074ab240d003d0183b2286716a40b
Solya-fk <olgasmalko2006@gmail.com>
2025-05-05 22:52:39 +0300

    Removed extra spaces

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const readline = require('readline');
 const fs = require('fs');
-const loadConfig = require('./laba5'); // Імпортуємо функцію для завантаження конфігурації
-const getDataFromApi = require('./api'); // Імпортуємо функцію для отримання даних з API
+const loadConfig = require('./laba5');
+const getDataFromApi = require('./api'); 
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -10,23 +10,22 @@ const rl = readline.createInterface({
 
 async function main() {
     try {
-        // Завантажуємо конфігурацію
+
         const config = await loadConfig('config.json');
         const apiKey = config.api_key;
 
-        // Запитуємо користувача
+       
         rl.question('Введіть параметр пошуку: ', async (query) => {
-            // Отримуємо дані з API
+        
             const data = await getDataFromApi(`https://jsonplaceholder.typicode.com/posts`, apiKey);
 
-            // Виводимо перші 3 результати
             console.log('Перші 3 результати:');
             data.slice(0, 5).forEach(item => {
                 console.log(`Заголовок: ${item.title}`);
                 console.log(`Опис: ${item.body}\n`);
             });
 
-            // Зберігаємо результат у output.json
+          
             fs.writeFile('output.json', JSON.stringify(data, null, 2), (err) => {
                 if (err) throw err;
                 console.log('Дані збережено у output.json');
